7ac2856ba6ea70da6c29f0befb1b2e36b4ac323d
Darksiend <logosmk2@gmail.com>
2020-07-05 22:55:42 +0300

    Add functionality to 'Add new'

--- client/frontend.js
+++ client/frontend.js
@@ -7,12 +7,19 @@ new Vue({
             form: {
                 name: '',
                 value: ' '
-            }
+            },
+            contacts: []
         }
     },
     methods: {
         createContact() {
-            console.log(this.form)
+            const {...contact} = this.form
+            this.contacts.push({...contact, id: Date.now()})
+
+            this.form.name = this.form.value = ''
+            console.log(this.contacts)
+
+
         }
     }
 })
