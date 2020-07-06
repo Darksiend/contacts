2c6256d8ba7e3c9e345d92afa016965590108e5a
Darksiend <logosmk2@gmail.com>
2020-07-06 12:48:33 +0300

    Add work with server

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const {v4} = require('uuid')
+
+let CONTACTS = [
+    {id: v4(), name: 'Anton', value: '0539861078', marked: false}
+]
+
+app.use(express.json())
+
+
+app.get('/api/contacts', (req, res) => {
+    setTimeout(() => {
+        res.status(200).json(CONTACTS)
+    }, 1000)
+
+})
+
+
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact)
+
+    res.status(201).json(contact)
+})
+
+app.delete('/api/contacts/:id', (req, res) => {
+
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({message: 'Contanct been deleted'})
+
+})
+
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[idx] = req.body
+    res.json(CONTACTS[idx])
+
+})
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
